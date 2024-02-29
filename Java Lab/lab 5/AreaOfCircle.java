5f1d6386d79e5d295e10f5457ad4d5bcec34c349
Prashamtogadiya <pktogadiya222@gmail.com>
2024-02-29 17:59:26 +0530

    Updated Code of Java and WD

---
+++ Java Lab/lab 5/AreaOfCircle.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class AreaOfCircle {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter radius of circle : ");
+        int radius = sc.nextInt();
+
+        double pi = 3.142, area;
+        area = pi * radius * radius;
+        System.out.println("Area of circle is :" + area);
+    }
+}
